b993705aabe048139be084d9f615eba617c415b1
Liana Gasparyan <lianagasparyan@LianaGasparyan-QT.local>
2022-09-10 11:39:21 +0400

    PRIMITIVE TYPESS

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+public class Main {
+    public static void main(String[] args) {
+        byte b1 = 11;
+        short s = 259;
+        int i = 1234;
+        long l = 9223372036854775807L;
+        float f;
+        f = 12.5F;
+        double d;
+        d = 12.77;
+        char ch1 = 'Y';
+        char ch2 = 'L';
+        boolean b;
+        b = true;
+        boolean q;
+        q = false;
+        // byte b1 = 12;
+        System.out.println("b1 is: " + b1);
+        System.out.println("s is: " + s);
+        System.out.println("i is: " + i);
+        System.out.println("l is: " + l);
+        System.out.println("f is: " + f);
+        System.out.println("ch1 is: " + ch1);
+        System.out.println("b is: " + b);
+        System.out.println("q is: " + q);
+
+
+    }
+}
