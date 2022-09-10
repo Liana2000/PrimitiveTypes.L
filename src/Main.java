9cb1c36d95c4ae2bc4e3924a0e10e6f20a7dc5fd
lianagasparyan <liana.gaspariann@gmail.com>
2022-09-10 17:16:40 +0400

    1

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,41 @@
 public class Main {
     public static void main(String[] args) {
-        byte b1 = 11;
-        short s = 259;
-        int i = 1234;
+        byte b1;
+        b1 = -127;
+        short s = 32767;
+        int i;
+        i = -2147483647;
         long l = 9223372036854775807L;
-        float f;
-        f = 12.5F;
+        float f = 12.5F;
+        float f1=30.5F;
         double d;
         d = 12.77;
         char ch1 = 'Y';
         char ch2 = 'L';
+        char ch3 = 65;
+        char ch4 = 66;
         boolean b;
         b = true;
         boolean q;
         q = false;
-        // byte b1 = 12;
-        System.out.println("b1 is: " + b1);
+        char chh = '1';
+        char ch10 = 50;
+
         System.out.println("s is: " + s);
+        System.out.println("b1 is: " + b1);
         System.out.println("i is: " + i);
         System.out.println("l is: " + l);
         System.out.println("f is: " + f);
+        System.out.println("f1 is: " + f1);
         System.out.println("ch1 is: " + ch1);
+        System.out.println("ch2 is: " + ch2);
+        System.out.println("ch3 is: " + ch3);
+        System.out.println("ch4 is: " + ch4);
         System.out.println("b is: " + b);
         System.out.println("q is: " + q);
+        System.out.println("ch10 is: " + ch10);
+        System.out.println("chh is: " + chh);
+
 
 
     }
